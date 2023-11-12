c34a98d757bb1110b22a8775b97c082079ac43c0
Marco Lecona <marcolecona@gmail.com>
2023-11-12 17:55:53 -0600

    Corecciones varias-r1.5

--- paginaWeb/settings.py
+++ paginaWeb/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     # manejo de archivos static
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     # aplicaciones del proyecto
     'Apps.pywebApp',
@@ -159,13 +160,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.

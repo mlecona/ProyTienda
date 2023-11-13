e9d34e7030ec6a1eeedb98ca784e197ab263e00c
Marco Lecona <marcolecona@gmail.com>
2023-11-13 17:43:21 -0600

    Correccion 1.1

--- paginaWeb/settings.py
+++ paginaWeb/settings.py
@@ -10,43 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 # Variables de entorno
 from decouple import config
-# Para despliegue
-import os
-# para despliegue instalar    pip install dj-database-url psycopg2-binary
-import dj_database_url
 
 # Para identificar msj de error
 from django.contrib.messages import constants as msj_error
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = config("SECRET_KEY")
-# Para despliegue
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
-# SECURITY WARNING: don't run with debug turned on in production!
-# Se cambia cuando esta en Modo Producción a False
-DEBUG = config("DEBUG", cast=bool)
+SECRET_KEY = config("SECRET_KEY")
 
-# Para despliegue
-#DEBUG = 'RENDER' not in os.environ
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
-# Para despliegue
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:    
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
 # Application definition
+
 BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -70,8 +57,6 @@ LOCAL_APPS = [
 ]
 
 THIRD_APPS = [
-    # manejo de archivos static
-    'whitenoise.runserver_nostatic',
     # Para manejo Autenticación
     'crispy_forms',
     'crispy_bootstrap5',
@@ -83,7 +68,6 @@ INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',   # Para despliegue
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +100,7 @@ WSGI_APPLICATION = 'paginaWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = { # Para despliegue
+DATABASES = {
     'default': 
         #{'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3'},
@@ -166,21 +150,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-if not DEBUG:
-    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Turn on WhiteNoise storage backend that takes care of compressing static files
-# and creating unique names for each version so they can safely be cached forever.
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
